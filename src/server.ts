19834888dd6ab1408e1bbfc88b895c79ce740aef
SM Anas <smanas.net@gmail.com>
2025-12-12 00:56:45 +0600

    deploy

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: process.env.CLIENT_URL || "http://localhost:3000",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
